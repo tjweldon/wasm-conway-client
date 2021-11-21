0ca910cb6d11b5b20a0cc72073184605860b3172
Tom Weldon <thomas.weldon@legl.com>
2021-11-20 22:14:24 -0500

    Added intercativity in the form of play/pause and toggling cells

--- index.js
+++ index.js
@@ -71,13 +71,60 @@ const drawCells = () => {
     ctx.stroke();
 };
 
-const renderLoop = () => {
-    universe.tick();
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+    universe.toggle_cell(row, col);
+
+    drawGrid();
     drawCells();
-    requestAnimationFrame(renderLoop);
+});
+
+
+const isPaused = () => {
+    return animationId === null;
 };
 
-drawGrid();
-drawCells();
-requestAnimationFrame(renderLoop);
+let animationId = null;
+const renderLoop = () => {
+    drawGrid();
+    drawCells();
 
+    universe.tick();
+
+    animationId = requestAnimationFrame(renderLoop);
+};
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    drawGrid();
+    drawCells();
+    animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
+
+play();
